de8399a7ba0cfdb0e9e776b0dd76c2988c82351c
ly <15901839273@163.com>
2018-09-05 09:53:20 +0800

    测试上传文件

--- inc/db_connect.php
+++ inc/db_connect.php
@@ -4,11 +4,11 @@
 // USE数据库
 class DB_COM extends Mysql {
 
-  public $schema = 'new22_hivebanks';
-  protected $server = '43.255.154.34';
-  protected $user = 'hive_admin';
+  public $schema = 'hb_agent_service';
+  protected $server = '47.254.40.100';
+  protected $user = 'admin';
   protected $password = 'l422ly8hgd4cluk8s7';
-  protected $database = 'new22_hivebanks';
+  protected $database = 'hb_agent_service';
   protected $character = 'utf8mb4';
 
 }
