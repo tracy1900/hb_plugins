5fb0d9f602e0ec72eb276091e34e75c1f5cb2b73
ly <15901839273@163.com>
2018-09-05 17:12:47 +0800

    邮件发送

--- plugin/email/send_email.php
+++ plugin/email/send_email.php
@@ -19,11 +19,11 @@ function send_email($name='', $email, $title, $body)
   $mail->SMTPAuth   = true;
   $mail->SMTPSecure = 'ssl';
   $mail->Host       = 'smtp.163.com';
-  $mail->Port       = 25;
+  $mail->Port       = 465;
   $mail->Username   = '"15901839273@163.com"';
   $mail->Password   = "Windwin2018";
   $mail->CharSet    = "utf-8";
-    $mail->SMTPDebug = 2;
+//    $mail->SMTPDebug = 2;
   $mail->SetFrom('15901839273@163.com', '上海风赢网络科技有限公司');
   $mail->Subject    = $title;
   $mail->MsgHTML($body);
