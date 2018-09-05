8cf5735a2f08ea4f5f974910bf615b1925e2b267
ly <15901839273@163.com>
2018-09-05 16:26:02 +0800

    邮件发送

--- email/send_email.php
+++ email/send_email.php
@@ -11,13 +11,13 @@ require_once '../plugin/email/send_email.php';
 
 require_once "../inc/common.php";
 
-$timestamp = time();
-$title = '邮箱验证链接';
-$des = new Des();
-$body = "www.baidu.com" . "?cfm_hash=";
-$encryption_code = "1".','."15901839273@163.com".',' . $timestamp .',' ."11";
-$body .=urlencode($des -> encrypt($encryption_code, "windwin"));
-$ret = send_email($name='', "15901839273@163.com", $title, $body);
+//$timestamp = time();
+//$title = '邮箱验证链接';
+//$des = new Des();
+//$body = "www.baidu.com" . "?cfm_hash=";
+//$encryption_code = "1".','."15901839273@163.com".',' . $timestamp .',' ."11";
+//$body .=urlencode($des -> encrypt($encryption_code, "windwin"));
+$ret = send_email($name='', "15901839273@163.com", "11", "222");
 if(!$ret){
     exit_error('124','邮件发送失败请稍后重试！');
 }
