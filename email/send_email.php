11a4e9dc741649defae9056ee65040ca28334ec5
ly <15901839273@163.com>
2018-09-06 15:48:08 +0800

    邮件发送

--- email/send_email.php
+++ email/send_email.php
@@ -11,7 +11,15 @@ require_once '../plugin/email/send_email.php';
 
 require_once "../inc/common.php";
 
-$ret = send_email($name='1646', "1378333642@qq.com", "祖国", get_guid());
+
+$args = array('email','title','body');
+chk_empty_args('POST', $args);
+$email = get_arg_str('POST', 'email');
+$title = get_arg_str('POST', 'title');
+$body = get_arg_str('POST', 'body');
+
+
+$ret = send_email($name='1646', $email, $title, $body);
 if(!$ret){
     exit_error('124','邮件发送失败请稍后重试！');
 }
