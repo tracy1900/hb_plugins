0dd516105e56c38f7c847ee2aae25947481a36fa
ly <15901839273@163.com>
2018-09-06 17:02:10 +0800

    邮件发送

--- email/send_email.php
+++ email/send_email.php
@@ -21,39 +21,3 @@ $body = get_arg_str('POST', 'body');
 print_r('\n'.$body);
 die;
 
-
-
-$callback = array();
-$ret = send_email($name='', $email, $title, $body);
-
-$log_data = array();
-
-if(!$ret){
-
-    $data_log['status'] = 1;
-    $data_log['log_id'] = get_guid();
-    $data_log['la_id'] = 'email';
-    $data_log['action_id'] = 'action_id';
-    $data_log['email'] = $email;
-    $data_log['ctime'] = time();
-    action_log_email($data_log);
-
-    $callback["errcode"] = '1';
-    $callback['errmsg'] = "邮件发送失败请稍后重试！";
-    echo json_encode($callback);
-    exit();
-}
-$callback = array();
-
-$data_log['status'] = 0;
-$data_log['log_id'] = get_guid();
-$data_log['la_id'] = 'email';
-$data_log['action_id'] = 'action_id';
-$data_log['email'] = $email;
-$data_log['ctime'] = time();
-action_log_email($data_log);
-
-$callback["errcode"] = '0';
-$callback['errmsg'] = "Please verify email as soon as possible!";
-echo json_encode($callback);
-exit();
