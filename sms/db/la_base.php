9aa936c57af9272d2e45f9071f6aae6c0c05656f
ly <15901839273@163.com>
2018-09-06 14:19:40 +0800

    邮件发送

---
+++ sms/db/la_base.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liangyi
+ * Date: 2018/9/5
+ * Time: 上午9:39
+ */
+function get_sms_la_base_info($la_id)
+{
+    $db = new DB_COM();
+    $sql = "SELECT * FROM la_base WHERE la_id = '{$la_id}' and type = 2";
+    $db->query($sql);
+    $rows = $db->fetchAll();
+    return $rows;
+}
+
+function ins_upload_file_service($data_base)
+{
+    $db = new DB_COM();
+    $sql = $db ->sqlInsert("la_base", $data_base);
+    $q_id = $db->query($sql);
+    if ($q_id == 0)
+        return false;
+    return true;
+}
---
+++ sms/set_sms_service.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liangyi
+ * Date: 2018/9/6
+ * Time: 上午10:14
+ */
+
+require_once "../inc/common.php";
+require_once "db/la_base.php";
+$args = array('la_id','type');
+chk_empty_args('POST', $args);
+$la_id = get_arg_str('POST', 'la_id');
+$type = get_arg_str('POST', 'type');
+
+$data = array();
+$data["la_id"] = $la_id;
+$data["type"] = $type;
+$data["status"] = 0;
+if(get_sms_la_base_info($la_id))
+    exit_error("1","请勿重复提交");
+
+if(!ins_upload_file_service($data))
+    exit_error("1","提交失败");
+else
+    exit_ok("提交成功，请等待审核");
+
+
