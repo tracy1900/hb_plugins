717ac42786192e89240670cc289b346f76a2246d
ly <15901839273@163.com>
2018-09-06 10:32:03 +0800

    邮件发送

---
+++ upload_file/get_upload_file_service.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liangyi
+ * Date: 2018/9/6
+ * Time: 上午10:27
+ */
+
+
+require_once "../inc/common.php";
+require_once "db/la_base.php";
+$args = array('la_id','type');
+chk_empty_args('POST', $args);
+$la_id = get_arg_str('POST', 'la_id');
+$type = get_arg_str('POST', 'type');
+
+$data = array();
+$data["la_id"] = $la_id;
+$data["type"] = $type;
+
+$row = get_upload_file_la_base_info($la_id);
+
+// 返回数据做成
+$rtn_ary = array();
+$rtn_ary['errcode'] = '0';
+$rtn_ary['errmsg'] = '';
+$rtn_ary['row'] = $row;
+
+$rtn_str = json_encode($rtn_ary);
+php_end($rtn_str);
