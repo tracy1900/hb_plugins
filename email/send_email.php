a480bbc18688484e132aab0b40898e064eed8b4e
ly <15901839273@163.com>
2018-09-06 17:02:39 +0800

    邮件发送

--- email/send_email.php
+++ email/send_email.php
@@ -5,6 +5,8 @@
  * Date: 2018/9/5
  * Time: 下午3:57
  */
+print_r(2222);
+
 ini_set('display_errors','On');
 Error_reporting(E_ALL);
 require_once '../plugin/email/send_email.php';
@@ -18,6 +20,6 @@ $email = get_arg_str('POST', 'email');
 $title = get_arg_str('POST', 'title');
 $body = get_arg_str('POST', 'body');
 
+print_r(1111);
 print_r('\n'.$body);
-die;
 
