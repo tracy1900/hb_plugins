ceac5b9790c73e69757c897967e40b624255bb14
范助国 <ahino@fanzhuguodeMacBook-Pro.local>
2018-09-06 16:48:00 +0800

    log_upload

--- email/log_email/log_email.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ahino
- * Date: 2018/9/6
- * Time: 下午2:21
- */
--- email/send_email.php
+++ email/send_email.php
@@ -8,6 +8,7 @@
 ini_set('display_errors','On');
 Error_reporting(E_ALL);
 require_once '../plugin/email/send_email.php';
+require_once 'db/log_email.php';
 
 require_once "../inc/common.php";
 
@@ -19,13 +20,33 @@ $body = get_arg_str('POST', 'body');
 
 $callback = array();
 $ret = send_email($name='1646', $email, $title, $body);
+
+$log_data = array();
+
 if(!$ret){
+
+    $data_log['status'] = 1;
+    $data_log['log_id'] = get_guid();
+    $data_log['la_id'] = 'email';
+    $data_log['action_id'] = 'action_id';
+    $data_log['email'] = $email;
+    $data_log['ctime'] = time();
+    action_log_email($data_log);
 
     $callback["errcode"] = '1';
     $callback['errmsg'] = "邮件发送失败请稍后重试！";
     echo json_encode($callback);
     exit();
 }
+
+$data_log['status'] = 0;
+$data_log['log_id'] = get_guid();
+$data_log['la_id'] = 'email';
+$data_log['action_id'] = 'action_id';
+$data_log['email'] = $email;
+$data_log['ctime'] = time();
+action_log_email($data_log);
+
 $callback["errcode"] = '0';
 $callback['errmsg'] = "Please verify email as soon as possible!";
 echo json_encode($callback);
