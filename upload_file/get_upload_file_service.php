0b9a245ae4e244866265a14f79c3835e632e9601
ly <15901839273@163.com>
2018-09-06 13:53:38 +0800

    邮件发送

--- upload_file/get_upload_file_service.php
+++ upload_file/get_upload_file_service.php
@@ -9,10 +9,9 @@
 
 require_once "../inc/common.php";
 require_once "db/la_base.php";
-$args = array('la_id','type');
+$args = array('la_id');
 chk_empty_args('POST', $args);
 $la_id = get_arg_str('POST', 'la_id');
-$type = get_arg_str('POST', 'type');
 
 $data = array();
 $data["la_id"] = $la_id;
